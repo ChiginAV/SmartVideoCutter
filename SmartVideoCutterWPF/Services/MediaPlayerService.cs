c9baec448e16540d2762234526ea71f1227ae213
Anton Chigin <chiginav@gmail.com>
2026-08-10 19:35:09 +0500

    вынес логику работы с libVlc из MainViewModel в MediaPlayerService

--- SmartVideoCutterWPF/Services/MediaPlayerService.cs
+++ SmartVideoCutterWPF/Services/MediaPlayerService.cs
@@ -1,5 +1,169 @@
-﻿namespace SmartVideoCutterWPF.Services;
+﻿using System.ComponentModel;
+using System.Windows.Threading;
+using LibVLCSharp.Shared;
+using MediaInfo;
 
-public class MediaPlayerService
+namespace SmartVideoCutterWPF.Services;
+
+public class MediaPlayerService : IDisposable
 {
+    #region Properties
+
+    private LibVLC? _libVlc;
+    private MediaPlayer? _mediaPlayer;
+    private DispatcherTimer? _timer;
+    private bool _isSeeking;
+    private bool _wasPlayingBeforeSeek;
+    private bool _isUpdatingFromTimer;
+    private bool _disposed;
+
+    private double _position;
+    private double _maxPosition;
+
+    public MediaPlayer? MediaPlayer => _mediaPlayer;
+
+    public double Position
+    {
+        get => _position;
+        private set
+        {
+            _position = value;
+            OnPropertyChanged(nameof(Position));
+        }
+    }
+
+    public double MaxPosition
+    {
+        get => _maxPosition;
+        private set
+        {
+            _maxPosition = value;
+            OnPropertyChanged(nameof(MaxPosition));
+        }
+    }
+
+    public bool IsPlaying => _mediaPlayer?.IsPlaying ?? false;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    #endregion
+
+    public void Initialize(Dispatcher dispatcher)
+    {
+        Core.Initialize();
+        _libVlc = new LibVLC();
+        _mediaPlayer = new MediaPlayer(_libVlc);
+
+        _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+        _timer.Tick += Timer_Tick;
+        _timer.Start();
+    }
+
+    public void LoadMedia(string filePath)
+    {
+        _timer?.Stop();
+        _mediaPlayer?.Stop();
+
+        if (_libVlc != null)
+        {
+            using var media = new Media(_libVlc, new Uri(filePath));
+            _mediaPlayer!.Media = media;
+
+            Position = 0;
+            MaxPosition = new MediaInfoWrapper(filePath).Duration;
+
+            _timer.Start();
+        }
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #region Player
+
+    public void Play() => _mediaPlayer?.Play();
+
+    public void Pause() => _mediaPlayer?.Pause();
+
+    public void PlayPause()
+    {
+        if (IsPlaying)
+            Pause();
+        else
+            Play();
+    }
+
+    public void BeginSeek()
+    {
+        _isSeeking = true;
+        _wasPlayingBeforeSeek = IsPlaying;
+
+        if (_wasPlayingBeforeSeek)
+            Pause();
+    }
+
+    public void EndSeek()
+    {
+        _isSeeking = false;
+
+        if (_wasPlayingBeforeSeek && _mediaPlayer != null)
+            Play();
+    }
+
+    public void SetPosition(double position)
+    {
+        Position = position;
+
+        if (!_isUpdatingFromTimer && _mediaPlayer != null)
+        {
+            _mediaPlayer.Time = (long)position;
+        }
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        if (!_isSeeking && _mediaPlayer != null && _mediaPlayer.IsPlaying)
+        {
+            _isUpdatingFromTimer = true;
+            Position = _mediaPlayer.Time;
+            _isUpdatingFromTimer = false;
+        }
+    }
+
+    #endregion
+
+    #region Dispose
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed) return;
+
+        if (disposing)
+        {
+            _timer?.Tick -= Timer_Tick;
+            _timer?.Stop();
+
+            _mediaPlayer?.Stop();
+            _mediaPlayer?.Dispose();
+
+            _libVlc?.Dispose();
+        }
+
+        _disposed = true;
+    }
+
+    ~MediaPlayerService()
+    {
+        Dispose(false);
+    }
+
+    #endregion
 }
--- SmartVideoCutterWPF/ViewModels/MainViewModel.cs
+++ SmartVideoCutterWPF/ViewModels/MainViewModel.cs
@@ -1,9 +1,9 @@
-﻿using System.Windows.Threading;
+﻿using System.ComponentModel;
+using System.Windows.Threading;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LibVLCSharp.Shared;
 using Microsoft.Win32;
-using MediaInfo;
 
 namespace SmartVideoCutterWPF.ViewModels;
 
@@ -11,34 +11,32 @@ public partial class MainViewModel : ObservableObject, IDisposable
 {
     #region Properties
 
-    // << LibVLCSharp
-    private readonly LibVLC? _libVlc;
-    [ObservableProperty] private MediaPlayer? _mediaPlayer;
+    private readonly MediaPlayerService _mediaPlayerService;
+    private readonly YoloFaceDetector? _faceDetector;
+    private bool _disposed;
+
     [ObservableProperty] private double _position;
     [ObservableProperty] private double _maxPosition;
-    private readonly DispatcherTimer _timer;
-    private bool _isSeeking;
-    private bool _wasPlayingBeforeSeek;
-    private bool _isUpdatingFromTimer;
-    // LibVLCSharp >>
 
-    private readonly YoloFaceDetector? _faceDetector;
-
-    private bool _disposed;
+    public MediaPlayer? MediaPlayer => _mediaPlayerService.MediaPlayer;
 
     #endregion
 
     public MainViewModel()
     {
-        Core.Initialize(); // Инициализация нативных библиотек VLC
-        _libVlc = new LibVLC();
-        MediaPlayer = new MediaPlayer(_libVlc);
-
-        _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
-        _timer.Tick += Timer_Tick;
-        _timer.Start();
+        _mediaPlayerService = new MediaPlayerService();
+        _mediaPlayerService.Initialize(Dispatcher.CurrentDispatcher);
+        _mediaPlayerService.PropertyChanged += MediaPlayerService_OnPropertyChanged;
 
         _faceDetector = new YoloFaceDetector();
+    }
+
+    private void MediaPlayerService_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MediaPlayerService.Position))
+            Position = _mediaPlayerService.Position;
+        else if (e.PropertyName == nameof(MediaPlayerService.MaxPosition))
+            MaxPosition = _mediaPlayerService.MaxPosition;
     }
 
     [RelayCommand]
@@ -47,26 +45,14 @@ public partial class MainViewModel : ObservableObject, IDisposable
         var dlg = new OpenFileDialog { Filter = "Video files|*.avi;*.mov;*.mkv;*.mp4" };
         if (dlg.ShowDialog() == true)
         {
-            _timer.Stop();
-            MediaPlayer.Stop();
-
-            using var media = new Media(_libVlc, new Uri(dlg.FileName));
-
-            MediaPlayer.Media = media;
-
-            Position = 0;
-            MaxPosition = new MediaInfoWrapper(dlg.FileName).Duration;
-
-            _timer.Start();
+            _mediaPlayerService.LoadMedia(dlg.FileName);
         }
     }
 
     [RelayCommand]
     private void OpenSettings()
     {
-        var settingsViewModel = new SettingsViewModel();
         var settingsWindow = new SettingsWindow();
-
         settingsWindow.ShowDialog();
     }
 
@@ -75,63 +61,25 @@ public partial class MainViewModel : ObservableObject, IDisposable
     [RelayCommand]
     private void PlayPause()
     {
-        if (MediaPlayer.IsPlaying)
-        {
-            MediaPlayer.Pause();
-        }
-        else
-        {
-            MediaPlayer.Play();
-        }
+        _mediaPlayerService.PlayPause();
     }
 
     [RelayCommand]
     private void DragStarted()
     {
-        _isSeeking = true;
-
-        _wasPlayingBeforeSeek = MediaPlayer.IsPlaying; // Запоминаем текущее состояние: играл плеер или стоял на паузе
-
-        if (_wasPlayingBeforeSeek)
-        {
-            MediaPlayer.Pause(); // Ставим на паузу, чтобы звук не заикался при перемещении
-        }
+        _mediaPlayerService.BeginSeek();
     }
 
     [RelayCommand]
     private void DragCompleted()
     {
-        _isSeeking = false;
-
-        // Возвращаем плееру то состояние, которое было до начала перемотки
-        if (_wasPlayingBeforeSeek && MediaPlayer != null)
-        {
-            MediaPlayer.Play();
-        }
-    }
-
-    private void Timer_Tick(object sender, EventArgs e)
-    {
-        // Опрашиваем плеер ТОЛЬКО если пользователь не занят перемоткой прямо сейчас
-        if (!_isSeeking && MediaPlayer != null && MediaPlayer.IsPlaying)
-        {
-            _isUpdatingFromTimer = true;
-
-            _position = MediaPlayer.Time;
-
-            OnPropertyChanged(nameof(Position));
-
-            _isUpdatingFromTimer = false;
-        }
+        _mediaPlayerService.EndSeek();
     }
 
     // Этот метод срабатывает КАЖДЫЙ РАЗ, когда пользователь сдвигает ползунок (даже на 1 пиксель)
     partial void OnPositionChanged(double value)
     {
-        if (!_isUpdatingFromTimer && MediaPlayer != null)
-        {
-            MediaPlayer.Time = (long)value;
-        }
+        _mediaPlayerService.SetPosition(value);
     }
 
     #endregion
@@ -161,40 +109,13 @@ public partial class MainViewModel : ObservableObject, IDisposable
 
         if (disposing)
         {
-            // Останавливаем и уничтожаем таймер
-            if (_timer != null)
-            {
-                _timer.Tick -= Timer_Tick; // Отписываемся от события
-                _timer.Stop(); // Останавливаем поток таймера
-            }
+            _mediaPlayerService.PropertyChanged -= MediaPlayerService_OnPropertyChanged;
+            _mediaPlayerService.Dispose();
 
-            // Освобождаем MediaPlayer
-            if (_mediaPlayer != null)
-            {
-                if (_mediaPlayer.IsPlaying)
-                {
-                    _mediaPlayer.Stop();
-                }
-
-                // Отписываемся от событий MediaPlayer, если вы их добавляли (например, EndReached, TimeChanged)
-                // _mediaPlayer.TimeChanged -= MediaPlayer_TimeChanged; 
-
-                _mediaPlayer.Dispose();
-            }
-
-            // Освобождаем сам контекст LibVLC (строго ПОСЛЕ плеера)
-            _libVlc?.Dispose();
-
-            //_faceDetector?.Dispose();
+            _faceDetector?.Dispose();
         }
 
         _disposed = true;
-    }
-
-    // Деструктор на случай, если Dispose не был вызван вручную
-    ~MainViewModel()
-    {
-        Dispose(false);
     }
 
     #endregion
