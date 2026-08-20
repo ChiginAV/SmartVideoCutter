c71b233c2386d76c6391040bb6f24dd9b4002579
Anton Chigin <chiginav@gmail.com>
2026-08-20 22:53:24 +0500

    удаление устаревшего проекта

--- SmartVideoCutter/Models/VideoSegment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SmartVideoCutter.Models
-{
-    public class VideoSegment
-    {
-        public int StartFrame { get; set; }
-        public int EndFrame { get; set; }
-    }
-}
--- SmartVideoCutter/Services/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using SmartVideoCutter.Models;
-global using SmartVideoCutter.Services;
-global using SmartVideoCutter.Services.FaceDetection;
-global using SmartVideoCutter.Services.FaceRecognition;
-global using SmartVideoCutter.Services.Ffmpeg;
-global using SmartVideoCutter.Services.Preprocessing;
-global using SmartVideoCutter.ViewModels;
-global using SmartVideoCutter.Views;
